e606f104993d61f10d4782ca1136799fdd69fb8c
mahones <sittisam2@gmail.com>
2025-12-22 16:28:30 +0000

    feat(users): refactor UsersList component and integrate it into Home page

--- src/components/usersList.tsx
+++ src/components/usersList.tsx
@@ -1,3 +1,15 @@
-"use client";
-
 import prisma from "@/lib/prisma";
+
+export default async function UsersList() {
+  const users = await prisma.users.findMany();
+
+  return (
+    <ol className="list-decimal list-inside font-[family-name:var(--font-geist-sans)]">
+      {users.map((user) => (
+        <li key={user.id} className="mb-2 text-[#555555]">
+          {user.name}
+        </li>
+      ))}
+    </ol>
+  );
+}
